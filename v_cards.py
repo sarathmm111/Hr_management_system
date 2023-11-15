e5f111fcf7b7b4e1573ff26e23c7a8d578d5a614
sarath <sarathmm224@gmail.com>
2023-11-15 13:11:41 +0530

    add generate v_cards funtion

--- v_cards.py
+++ v_cards.py
@@ -3,7 +3,6 @@ import os
 import csv
 
 
-
 def generate_vcard(lname, fname, title, email, phone):
     vcard = f"""BEGIN:VCARD
 VERSION:2.1
@@ -18,4 +17,22 @@ REV:20150922T195243Z
 END:VCARD
 """
     return vcard
+
+def genarate_vcards(output_directory="v_cards"):
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    with open("names.csv", newline="") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for lname, fname, title, email, phone in csv_reader:
+            vcard = generate_vcard(lname, fname, title, email, phone)
+            vcard_filename = f"{fname[:1]}_{lname}.vcf"
+            vcard_path = os.path.join(output_directory, vcard_filename)
+
+            with open(vcard_path, "w") as vcard_file:
+                vcard_file.write(vcard)
+                
+if __name__ == "__main__":
+    genarate_vcards()
+
 
