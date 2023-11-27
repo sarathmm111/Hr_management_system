8a0d9ed298b601331d52597e0b29653b40dbac4d
sarath <sarathmm224@gmail.com>
2023-11-27 11:48:19 +0530

    add designation sql file

---
+++ designation.sql
@@ -0,0 +1,5 @@
+INSERT INTO designation (designation,num_of_leaves) VALUES ('Staff Engineer',20);
+INSERT INTO designation (designation,num_of_leaves) VALUES ('Senior Engineer',18);
+INSERT INTO designation (designation,num_of_leaves) VALUES ('Junior Engineer',12);
+INSERT INTO designation (designation,num_of_leaves) VALUES ('Technical Lead',12);
+INSERT INTO designation (designation,num_of_leaves) VALUES ('Project Manager',15);
