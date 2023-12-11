397ec98e59e28aad8a41a79e1bb3c8edb9dcef41
sarath <sarathmm224@gmail.com>
2023-12-11 11:28:03 +0530

    update web

--- web.py
+++ web.py
@@ -23,22 +23,40 @@ def employees():
 
 @app.route("/employees/<int:empid>", methods=["GET", "POST"])
 def employee_details(empid):
-  all_user_query = db.select(models.employee).order_by(models.employee.firstname)
-  all_user =db.session.execute(all_user_query).scalars()
   query = db.select(models.employee).where(models.employee.empid == empid)
   user = db.session.execute(query).scalar()
   query1 = db.select(func.count(models.employee.empid)).join(models.leaves,models.employee.empid == models.leaves.empid).where(models.employee.empid==empid)
   leaves = db.session.execute(query1).scalar()
   query2 = db.select(models.designation.max_leaves).where(models.designation.jobid == models.employee.title_id, models.employee.empid==empid)
   max_leaves = db.session.execute(query2).scalar()
+  ret = {"employee_id" : user.empid,
+         "firstname" : user.firstname,
+         "lastname" : user.lastname,
+         "title" : user.title.title,
+         "email" : user.email,
+         "phone" : user.ph_no,
+         "leaves": leaves,
+         "max_leaves" : max_leaves}
   if request.method == "POST":
     date = request.form['date']
     reason = request.form['reason'] 
     query3 = models.leaves(empid=empid ,date=date, reason=reason)
     db.session.add(query3)
     db.session.commit()
-    return redirect(url_for("employee_details",empid=empid))
-  return render_template("userdetails.html", user=user, leaves=leaves, max_leaves=max_leaves, all_user=all_user)
+    return redirect(url_for("employee_details"))         
+  return flask.jsonify(ret)
+
+@app.route("/addleave/<int:empid>", methods=["GET", "POST"])
+def addleave(empid):
+  query = db.select(models.employee).where(models.employee.empid == empid)
+  user = db.session.execute(query).scalar()
+  if request.method == "POST":
+    date = request.form['date']
+    reason = request.form['reason'] 
+    query3 = models.leaves(empid=empid ,date=date, reason=reason)
+    db.session.add(query3)
+    db.session.commit()
+    return redirect(url_for("employees"))
 
 @app.route("/about")
 def about():
