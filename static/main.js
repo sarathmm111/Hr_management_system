c4dda0ffd99b520f6f698722b44bcd9bfe2cd12c
sarath <sarathmm224@gmail.com>
2023-12-19 13:38:28 +0530

    add static

--- static/main.js
+++ static/main.js
@@ -1,55 +1,55 @@
-function gotEmployees(data) {
-  console.log(data);
-  $("#userdetails")[0].innerHTML=`<h1> Details for ${data.firstname}  ${data.lastname}</h1>
-  <h2> ${data.title} </h2>
-  <table>
-    <tr>
-      <th> First name </th>
-      <td> ${data.firstname}</td>
-    </tr>
-    <tr>
-      <th> Last name </th>
-      <td> ${data.lastname}</td>
-    </tr>
-    <tr>
-      <th> Email </th>
-      <td> ${data.email}</td>
-    </tr>
-    <tr>
-      <th> Phone </th>
-      <td> ${data.phone}</td>
-    </tr>
-    <tr>
-      <th> Number of leaves taken </th>
-        <td> ${data.leaves}</td>
-    </tr>
-    <tr>
-      <th>Maximum number of leaves</th>
-        <td> ${data.max_leaves}</td>
-    </tr>
-  </table>
-  <br>
-      <h1> Enter leave data for user </h1>
-<form class="leaveform" action = /addleave/${data.employee_id} onsubmit="showAlert()" method="post">
-  <label for="date">Date:</label>
-  <input type="date" name="date" required>
-  <br>
-  <label for="reason">Reason:</label>
-  <textarea id="reason" name="reason" rows="4" cols="10" required></textarea>
-  <br>
-  <input type="submit" value="Submit">
-</form>
-`;
+// function gotEmployees(data) {
+//   console.log(data);
+//   $("#userdetails")[0].innerHTML=`<h1> Details for ${data.firstname}  ${data.lastname}</h1>
+//   <h2> ${data.title} </h2>
+//   <table>
+//     <tr>
+//       <th> First name </th>
+//       <td> ${data.firstname}</td>
+//     </tr>
+//     <tr>
+//       <th> Last name </th>
+//       <td> ${data.lastname}</td>
+//     </tr>
+//     <tr>
+//       <th> Email </th>
+//       <td> ${data.email}</td>
+//     </tr>
+//     <tr>
+//       <th> Phone </th>
+//       <td> ${data.phone}</td>
+//     </tr>
+//     <tr>
+//       <th> Number of leaves taken </th>
+//         <td> ${data.leaves}</td>
+//     </tr>
+//     <tr>
+//       <th>Maximum number of leaves</th>
+//         <td> ${data.max_leaves}</td>
+//     </tr>
+//   </table>
+//   <br>
+//       <h1> Enter leave data for user </h1>
+// <form class="leaveform" action = /addleave/${data.employee_id} onsubmit="showAlert()" method="post">
+//   <label for="date">Date:</label>
+//   <input type="date" name="date" required>
+//   <br>
+//   <label for="reason">Reason:</label>
+//   <textarea id="reason" name="reason" rows="4" cols="10" required></textarea>
+//   <br>
+//   <input type="submit" value="Submit">
+// </form>
+// `;
 
-}
+// }
 
-function showAlert() {
-  alert("Submitted leave form successfully."); 
-}
+// function showAlert() {
+//   alert("Submitted leave form successfully."); 
+// }
 
-$(function() {
-  $("a.userlink").click(function (ev) {
-      $.get(ev.target.href, gotEmployees);
-      ev.preventDefault();
-      });
-});
+// $(function() {
+//   $("a.userlink").click(function (ev) {
+//       $.get(ev.target.href, gotEmployees);
+//       ev.preventDefault();
+//       });
+// });
