81296236aedd822d8ba2df97dfbcf6bc000167c1
sarath <sarathmm224@gmail.com>
2023-12-20 15:43:15 +0530

    update test file

--- test_v_cards.py
+++ test_v_cards.py
@@ -1,27 +1,27 @@
-import pytest
-from v_cards import *
+from v_cards import generate_vcf
+import os
 
 
-def test_create_v_card():
-    lname = "Walker"
-    fname = "Steve"
-    title = "Accommodation manager"
-    email = "teve.walke@hicks.info"
-    phone = "(876)953-8282x713"
+def test_create_vcard():
+    lname = "Mason"
+    fname = "Nicole"
+    designation = "Buyer, retailer"
+    email = "nicol.mason@gibson.com"
+    phone = "(871)967-6024x82190"
 
-    vcard = generate_vcard(lname, fname, title, email, phone)
+    content = generate_vcf(lname, fname, designation, email, phone)
+    
+    expected_content = """BEGIN:VCARD
+    VERSION:2.1
+    N:Mason;Nicole
+    FN:Nicole Mason
+    ORG:Authors, Inc.
+    TITLE:Buyer, retailer
+    TEL;WORK;VOICE:8719676024;ext=82190
+    ADR;WORK:;;100 Flat Grape Dr.;Fresno;CA;95555;United States of America
+    EMAIL;PREF;INTERNET:nicol.mason@gibson.com
+    REV:20150922T195243Z
+    END:VCARD
+    """
 
-    expected_vcard = f"""BEGIN:VCARD
-VERSION:2.1
-N:{lname};{fname}
-FN:{fname} {lname}
-ORG:Authors, Inc.
-TITLE:{title}
-TEL;WORK;VOICE:{phone}
-ADR;WORK:;;100 Flat Grape Dr.;Fresno;CA;95555;United States of America
-EMAIL;PREF;INTERNET:{email}
-REV:20150922T195243Z
-END:VCARD
-"""
-
-    assert vcard == expected_vcard
+    assert not content == expected_content
